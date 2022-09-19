d798fba697d3b4a1455f89f166993e274f1be025
Marco Fornaciari <mfornaciari@gmail.com>
2022-09-18 21:29:28 -0300

    style: rename mocked function

--- tests/InputNumber.test.tsx
+++ tests/InputNumber.test.tsx
@@ -6,8 +6,8 @@ import InputNumber from '../src/components/InputNumber';
 
 describe('InputNumber', () => {
   it('does not allow user to type in non-numeric characters', async () => {
-    const onChangeHandlerMock = jest.fn();
-    render(<InputNumber labelText='test' setFormValue={onChangeHandlerMock} />)
+    const setFormValueMock = jest.fn();
+    render(<InputNumber labelText='test' setFormValue={setFormValueMock} />)
     const input = screen.getByRole('spinbutton', { name: 'test' });
 
     await userEvent.type(input, '!-.,1a2');
