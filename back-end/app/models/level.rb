06ca3ed85eb48b407c6735233d23f360dc41351f
Marco Fornaciari <mfornaciari@gmail.com>
2022-09-23 16:21:56 -0300

    add level validations

--- back-end/app/models/level.rb
+++ back-end/app/models/level.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Level < ApplicationRecord
+  validates :level, :min_experience, :max_experience, presence: true
+  validates :level, :min_experience, :max_experience, uniqueness: true
 end
--- back-end/spec/models/level_spec.rb
+++ back-end/spec/models/level_spec.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Level, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:level) { create :level, level: 1, min_experience: 0, max_experience: 300 }
+
+  it { is_expected.to validate_presence_of(:level) }
+  it { is_expected.to validate_presence_of(:min_experience) }
+  it { is_expected.to validate_presence_of(:max_experience) }
+  it { is_expected.to validate_uniqueness_of(:level) }
+  it { is_expected.to validate_uniqueness_of(:min_experience) }
+  it { is_expected.to validate_uniqueness_of(:max_experience) }
 end
