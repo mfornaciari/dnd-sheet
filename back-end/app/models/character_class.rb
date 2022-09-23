bed8f3a4683714551beda1ab2426f714a4a890b6
Marco Fornaciari <mfornaciari@gmail.com>
2022-09-23 08:20:51 -0300

    feat: validate character class attributes

--- back-end/app/models/character_class.rb
+++ back-end/app/models/character_class.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class CharacterClass < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- back-end/spec/models/character_class_spec.rb
+++ back-end/spec/models/character_class_spec.rb
@@ -3,5 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe CharacterClass, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:character_class) { create :character_class, name: 'Bardo' }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
 end
