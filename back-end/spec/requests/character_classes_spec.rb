615b5f38f8291524b44c998547c1421166701ea0
Marco Fornaciari <mfornaciari@gmail.com>
2022-09-23 09:41:42 -0300

    test: check fetching of single class data

--- back-end/spec/requests/character_classes_spec.rb
+++ back-end/spec/requests/character_classes_spec.rb
@@ -6,18 +6,41 @@ describe 'POST /graphql' do
   it 'returns all classes' do
     class_names = %w[Bárbaro Bardo Bruxo]
     class_names.each { |name| create :character_class, name: name }
+    class1, class2, class3 = CharacterClass.where(name: class_names)
     expected_response = {
       data: {
         characterClasses: [
-          { id: '1', name: 'Bárbaro' },
-          { id: '2', name: 'Bardo' },
-          { id: '3', name: 'Bruxo' }
+          { id: class1.id.to_s, name: 'Bárbaro' },
+          { id: class2.id.to_s, name: 'Bardo' },
+          { id: class3.id.to_s, name: 'Bruxo' }
         ]
       }
     }
 
-    post '/graphql', params: { query: 'query { characterClasses { name } }' }
+    post '/graphql', params: { query: 'query { characterClasses { id name } }' }
 
-    expect(JSON.parse(response.body, symbolize_names: true)).to eq(expected_response)
+    expect(format(response.body)).to eq(expected_response)
+  end
+
+  it 'finds a single class by ID and returns it' do
+    class_names = %w[Bárbaro Bardo Bruxo]
+    class_names.each { |name| create :character_class, name: name }
+    class1 = CharacterClass.find_by(name: 'Bárbaro')
+    expected_response = {
+      data: {
+        characterClass: {
+          id: class1.id.to_s,
+          name: 'Bárbaro'
+        }
+      }
+    }
+
+    post '/graphql', params: { query: "query { characterClass(id: #{class1.id}) { id name } }" }
+
+    expect(format(response.body)).to eq(expected_response)
+  end
+
+  def format(response_body)
+    JSON.parse(response_body, symbolize_names: true)
   end
 end
