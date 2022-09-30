2d6d07c73dd4894f8908004a248cf85a48883846
Marco Fornaciari <mfornaciari@gmail.com>
2022-09-30 10:50:10 -0300

    test save link

--- front-end/tests/Form.test.tsx
+++ front-end/tests/Form.test.tsx
@@ -158,29 +158,15 @@ describe('Form', () => {
     expect(localStorage.characterValues).toEqual(JSON.stringify(expectedValues));
   });
 
-  it.only('exports data to JSON file upon clicking the save button', async () => {
-    const mockURL = 'http://localhost:3000/mockURL';
-    const mockGenerateURL = () => mockURL;
-    jest.mock('@/services/generateURL', () => {
-      return {
-        __esModule: true,
-        default: jest.fn().mockImplementation(() => {
-          return {
-            generateURL: mockGenerateURL
-          }
-        })
-      }
-    });
-
+  it('exports data to JSON file upon clicking the save button', async () => {
     render(<TestForm />);
     await waitForElementToBeRemoved(() => screen.getByRole('status', { name: 'Carregando...' }));
     const nameInput: HTMLInputElement = screen.getByRole('textbox', { name: 'Nome'});
     const saveButton: HTMLAnchorElement = screen.getByRole('link', { name: 'Salvar' });
 
     await userEvent.type(nameInput, 'teste');
-    await userEvent.click(saveButton);
 
-    expect(saveButton).toHaveAttribute('href', mockURL);
+    expect(saveButton).toHaveAttribute('href', 'http://localhost:3000/mockURL');
     expect(saveButton).toHaveAttribute('download', 'teste');
   });
 });
--- front-end/tests/setup.ts
+++ front-end/tests/setup.ts
@@ -1,2 +1,6 @@
 import '@testing-library/jest-dom';
 import '@/services/i18n.ts';
+
+const mockGenerateUrl = jest.fn();
+jest.mock('@/services/generateURL', () => mockGenerateUrl);
+mockGenerateUrl.mockReturnValue('http://localhost:3000/mockURL');
