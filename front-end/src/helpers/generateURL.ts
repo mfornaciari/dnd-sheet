a0e0186060b4e226ffe98419070eb07aa293364a
Marco Fornaciari <mfornaciari@gmail.com>
2022-10-14 19:35:31 -0300

    Remove unused helper

--- front-end/src/helpers/generateURL.ts
+++
@@ -1,6 +0,0 @@
-import { CharacterValues } from "@/types";
-
-export function generateURL(formValues: CharacterValues) {
-  const blob = new Blob([JSON.stringify(formValues)], { type: 'application/json' });
-  return URL.createObjectURL(blob);
-}
