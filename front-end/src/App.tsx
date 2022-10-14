7ad83a970420e38416d670f432132e61b99aaa07
Marco Fornaciari <mfornaciari@gmail.com>
2022-10-14 19:41:46 -0300

    Move i18next to main.tsx

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,6 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import i18next from 'i18next';
-import ptbr from '../locales/pt-BR.json';
 import '@/style/App.css';
 import Form from '@/components/Form';
-
-i18next.init({
-  lng: 'ptbr',
-  debug: false,
-  resources: ptbr,
-});
 
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import i18next from 'i18next';
+import ptbr from '../locales/pt-BR.json';
 import App from '@/App';
+
+i18next.init({
+  lng: 'ptbr',
+  debug: false,
+  resources: ptbr,
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
