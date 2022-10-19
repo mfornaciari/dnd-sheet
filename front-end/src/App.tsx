abf318644902dc20947ae0a6cc73e51921fdf5e8
Marco Fornaciari <mfornaciari@gmail.com>
2022-10-19 19:28:32 -0300

    Extract querying to QueryWrapper component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,17 +1,9 @@
-import { useQuery } from '@apollo/client';
-import { GET_DATA } from './queries/getData';
-import { FetchedData } from './types';
+import type { FetchedData } from './types';
 import '@/style/App.css';
+import { GET_DATA } from './queries/getData';
+import { QueryWrapper } from '@/components/QueryWrapper';
 import { Form } from '@/components/Form';
-import { StatusMessage } from './components/StatusMessage';
 
 export default function App() {
-  const { loading, error, data } = useQuery<FetchedData>(GET_DATA);
-
-  if (loading) return <StatusMessage message='loading' />;
-  if (error || !data) return <StatusMessage message='error' />;
-
-  return (
-    <Form data={data} />
-  );
+  return <QueryWrapper query={GET_DATA}>{(data: FetchedData) => <Form data={data} />}</QueryWrapper>;
 }
--- front-end/src/components/QueryWrapper.tsx
+++ front-end/src/components/QueryWrapper.tsx
@@ -1,12 +1,17 @@
 import type { FetchedData } from '@/types';
-import { useQuery } from '@apollo/client';
-import { GET_DATA } from '@/queries/getData';
+import { DocumentNode, useQuery } from '@apollo/client';
 import { StatusMessage } from '@/components/StatusMessage';
-import { PropsWithChildren } from 'react';
 
-export function QueryWrapper() {
-  const { loading, error, data } = useQuery<FetchedData>(GET_DATA);
+type QueryWrapperProps = {
+  query: DocumentNode;
+  children: (data: FetchedData) => JSX.Element;
+};
+
+export function QueryWrapper({ query, children }: QueryWrapperProps) {
+  const { loading, error, data } = useQuery<FetchedData>(query);
 
   if (loading) return <StatusMessage message='loading' />;
   if (error || !data) return <StatusMessage message='error' />;
+
+  return children(data);
 }
