798acf834fec6e5c52528015b42b198921501d59
Marco Fornaciari <mfornaciari@gmail.com>
2022-10-29 21:36:42 -0300

    Test Form data storing and loading

--- front-end/src/app/components/Form/Form.test.tsx
+++ front-end/src/app/components/Form/Form.test.tsx
@@ -1,24 +1,62 @@
 import type { FetchedData } from "@/types";
 import { render, screen, within } from "@testing-library/react";
-import "./Form.css";
+import user from "@testing-library/user-event";
+import i18next from "i18next";
 import fetchedDataMock from "@/test/fetchedDataMock.json";
 import { Form } from "./Form";
 
 describe("Form", () => {
-  it("renders correctly", () => {
-    const data = fetchedDataMock.data as FetchedData;
+  afterEach(() => localStorage.clear());
 
+  it("saves entered data to localStorage", async () => {
+    const data = fetchedDataMock.data as FetchedData;
     render(<Form data={data} />);
-    const form = screen.getByRole("form", { name: "Formulário" });
-    const nameInput = within(form).getByRole("textbox", { name: "Nome" });
-    const raceInput = within(form).getByRole("combobox", { name: "Raça" });
-    const classInput = within(form).getByRole("combobox", { name: "Classe" });
-    const xpInput = within(form).getByRole("spinbutton", { name: "Experiência" });
-    const tabPanel = within(form).getByRole("tabpanel", { name: "Pessoal" });
-    const tabButtonPersonal = within(form).getByRole("tab", { name: "Pessoal" });
-    const tabButtonAttributes = within(form).getByRole("tab", { name: "Atributos" });
-    const tabButtonClass = within(form).getByRole("tab", { name: "Classe" });
-    const tabButtonSpells = within(form).getByRole("tab", { name: "Magias" });
-    const tabButtonItems = within(form).getByRole("tab", { name: "Itens" });
+    const form: HTMLFormElement = screen.getByRole("form", { name: "Formulário" });
+    const nameInput: HTMLInputElement = within(form).getByRole("textbox", { name: "Nome" });
+    const raceInput: HTMLInputElement = within(form).getByRole("combobox", { name: "Raça" });
+    const classInput: HTMLInputElement = within(form).getByRole("combobox", { name: "Classe" });
+    const xpInput: HTMLInputElement = within(form).getByRole("spinbutton", { name: "Experiência" });
+
+    await user.type(nameInput, "Bruenor");
+    await user.selectOptions(raceInput, data.races[0].name);
+    await user.selectOptions(classInput, i18next.t(data.characterClasses[0].name));
+    await user.type(xpInput, "300");
+
+    const expectedValues = JSON.stringify({
+      name: "Bruenor",
+      race: fetchedDataMock.data.races[0].id,
+      characterClass: fetchedDataMock.data.characterClasses[0].id,
+      experience: "300",
+    });
+    expect(localStorage.characterValues).toEqual(expectedValues);
+
+    //TODO: Test that form gives invalid inputs a red outline
+  });
+
+  it("loads data from file", async () => {
+    const data = fetchedDataMock.data as FetchedData;
+    render(<Form data={data} />);
+    const form: HTMLFormElement = screen.getByRole("form", { name: "Formulário" });
+    const nameInput: HTMLInputElement = within(form).getByRole("textbox", { name: "Nome" });
+    const raceInput: HTMLInputElement = within(form).getByRole("combobox", { name: "Raça" });
+    const classInput: HTMLInputElement = within(form).getByRole("combobox", { name: "Classe" });
+    const xpInput: HTMLInputElement = within(form).getByRole("spinbutton", { name: "Experiência" });
+    const loadButton: HTMLInputElement = within(form).getByRole("button", { name: "Carregar" });
+    const fileValues = JSON.stringify({
+      name: "Jozan",
+      race: fetchedDataMock.data.races[1].id,
+      characterClass: fetchedDataMock.data.characterClasses[1].id,
+      experience: "0",
+    });
+    const file = new File([fileValues], "Jozan.json", { type: "application/json" });
+    File.prototype.text = jest.fn().mockResolvedValue(fileValues);
+
+    await user.upload(loadButton, file);
+
+    expect(localStorage.characterValues).toEqual(fileValues);
+    expect(nameInput).toHaveDisplayValue("Jozan");
+    expect(raceInput).toHaveDisplayValue(fetchedDataMock.data.races[1].name);
+    expect(classInput).toHaveDisplayValue(i18next.t(fetchedDataMock.data.characterClasses[1].name));
+    expect(xpInput).toHaveDisplayValue("0");
   });
 });
--- front-end/src/test/setup.ts
+++ front-end/src/test/setup.ts
@@ -1,18 +1,18 @@
-import '@testing-library/jest-dom';
-import i18next from 'i18next';
-import ptbr from '@/locales/pt-BR.json';
-import { CharacterValues } from '@/types';
+import type { CharacterValues } from "@/types";
+import "@testing-library/jest-dom";
+import i18next from "i18next";
+import ptbr from "@/locales/pt-BR.json";
 
 i18next.init({
-  lng: 'ptbr',
+  lng: "ptbr",
   debug: false,
   resources: ptbr
 });
 
-const mockURL = 'http://localhost:3000/mockURL';
+const mockURL = "http://localhost:3000/mockURL";
 const mockGenerateURL = jest.fn((_formValues: CharacterValues) => mockURL);
-jest.mock('@/app/hooks/useCharacterForm/helpers/useCharacterFormHelpers', () => {
-  const original = jest.requireActual('@/app/hooks/useCharacterForm/helpers/useCharacterFormHelpers');
+jest.mock("@/app/hooks/useCharacterForm/helpers/useCharacterFormHelpers", () => {
+  const original = jest.requireActual("@/app/hooks/useCharacterForm/helpers/useCharacterFormHelpers");
 
   return {
     ...original,
