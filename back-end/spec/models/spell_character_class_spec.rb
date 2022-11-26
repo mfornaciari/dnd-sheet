833ef99b93a453d2bd39797fe544f4dc08a37011
Marco Fornaciari <mfornaciari@gmail.com>
2022-11-26 09:45:33 -0300

    Add join table spell_character_class validations

--- back-end/spec/models/spell_character_class_spec.rb
+++ back-end/spec/models/spell_character_class_spec.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe SpellCharacterClass, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe SpellCharacterClass do
+  it { is_expected.to belong_to(:spell) }
+  it { is_expected.to belong_to(:character_class) }
 end
