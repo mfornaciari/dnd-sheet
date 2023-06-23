0b9a4b58fe86fc239a9707ba838b50e09f22277a
Marco Fornaciari <mfornaciari@gmail.com>
2023-06-23 19:44:00 -0300

    refact spells panel helpers

--- front-end/src/app/components/panels/PanelSpells/PanelSpells.tsx
+++ front-end/src/app/components/panels/PanelSpells/PanelSpells.tsx
@@ -14,31 +14,27 @@ export function PanelSpells({ spells }: PanelSpellsProps): JSX.Element {
   const knownSpellsLevels = getSpellLevels(knownSpells);
   const allSpellsSectionNames = allSpellsLevels.map(level => `Nível ${level}`);
   const knownSpellsSectionNames = knownSpellsLevels.map(level => `Nível ${level}`);
-
-  function handleListItemClick(event: MouseEvent<HTMLLIElement>): void {
-    const spellName = event.currentTarget.textContent;
-    if (spellName !== null) {
-      const spell = spells.find(spell => spell.name === spellName);
-      if (spell !== undefined) {
-        setKnownSpells(prevKnownSpells => [spell, ...prevKnownSpells]);
-      }
-    }
-  }
-  function buildSpellListItemsByLevel(spellNamesByLevel: string[][]): JSX.Element[][] {
-    const spellListItemsByLevel = spellNamesByLevel.map(group =>
-      group.map(spellName => (
-        <li key={spellName}>
-          <ListItemClickable handleClick={handleListItemClick}>{spellName}</ListItemClickable>
-        </li>
-      ))
-    );
-    return spellListItemsByLevel;
-  }
-
   const allSpellNamesByLevel = getSpellNamesByLevel(spells, allSpellsLevels);
   const knownSpellNamesByLevel = getSpellNamesByLevel(knownSpells, knownSpellsLevels);
-  const allSpellsListItemsByLevel = buildSpellListItemsByLevel(allSpellNamesByLevel);
-  const knownSpellsListItemsByLevel = buildSpellListItemsByLevel(knownSpellNamesByLevel);
+
+  function addSpell(spell: Spell): void {
+    setKnownSpells(prevKnownSpells => [spell, ...prevKnownSpells]);
+  }
+  function removeSpell(spell: Spell): void {
+    setKnownSpells(prevKnownSpells => prevKnownSpells.filter(prevSpell => prevSpell.name !== spell.name));
+  }
+  function handleAllSpellsListItemClick(event: MouseEvent<HTMLLIElement>): void {
+    handleListItemClick(event, spells, addSpell);
+  }
+  function handleKnownSpellsListItemClick(event: MouseEvent<HTMLLIElement>): void {
+    handleListItemClick(event, knownSpells, removeSpell);
+  }
+
+  const allSpellsListItemsByLevel = buildSpellListItemsByLevel(allSpellNamesByLevel, handleAllSpellsListItemClick);
+  const knownSpellsListItemsByLevel = buildSpellListItemsByLevel(
+    knownSpellNamesByLevel,
+    handleKnownSpellsListItemClick
+  );
 
   return (
     <Panel className="panel-spells" tabButtonId="spells">
@@ -59,6 +55,10 @@ export function PanelSpells({ spells }: PanelSpellsProps): JSX.Element {
   );
 }
 
+function getSpellLevels(spells: Spell[]): SpellLevel[] {
+  return [...new Set(spells.map(spell => spell.level))].sort((item, otherItem) => item - otherItem);
+}
+
 function getSpellNamesByLevel(spells: Spell[], levels: SpellLevel[]): string[][] {
   const spellNamesByLevel = levels.map(level => {
     const currentLevelSpells = spells.filter(spell => spell.level === level);
@@ -67,6 +67,28 @@ function getSpellNamesByLevel(spells: Spell[], levels: SpellLevel[]): string[][]
   return spellNamesByLevel;
 }
 
-function getSpellLevels(spells: Spell[]): SpellLevel[] {
-  return [...new Set(spells.map(spell => spell.level))].sort((item, otherItem) => item - otherItem);
+function handleListItemClick(
+  event: MouseEvent<HTMLLIElement>,
+  spellList: Spell[],
+  action: (spell: Spell) => void
+): void {
+  const spellName = event.currentTarget.textContent;
+  if (spellName !== null) {
+    const spell = spellList.find(spell => spell.name === spellName);
+    if (spell !== undefined) action(spell);
+  }
+}
+
+function buildSpellListItemsByLevel(
+  spellNamesByLevel: string[][],
+  handleClick: (event: MouseEvent<HTMLLIElement>) => void
+): JSX.Element[][] {
+  const spellListItemsByLevel = spellNamesByLevel.map(group =>
+    group.map(spellName => (
+      <li key={spellName}>
+        <ListItemClickable handleClick={handleClick}>{spellName}</ListItemClickable>
+      </li>
+    ))
+  );
+  return spellListItemsByLevel;
 }
